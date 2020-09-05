5e2f91f69e288fb116e617e0d7f7ad4af52fd8ae
Marquis Bentley <livefreekai@icloud.com>
2020-09-05 13:18:24 -0700

    Create session api calls to login, logout and signup

---
+++ HelloStranger/src/util/session_api_util.js
@@ -0,0 +1,22 @@
+export const postSession = user => (
+    $.ajax({
+        method: 'post',
+        url: '/api/session/',
+        data: { user }
+    })
+);
+
+export const deleteSession = () => (
+    $.ajax({
+        method: 'delete',
+        url: '/api/session/'
+    })
+);
+
+export const createUser = user => (
+    $.ajax({
+        method: 'post',
+        url: '/api/users/',
+        data: { user }
+    })
+);
